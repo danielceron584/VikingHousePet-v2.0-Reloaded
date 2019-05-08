de373792c6298686ad106e502a134358c296dc13
danielceron584 <50185498+danielceron584@users.noreply.github.com>
2019-05-08 01:58:51 -0500

    v1.1
    Disqus added social media buttons added scroll on page added and other stuff, right now i can't remember at all.

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+
+
 let popUp = document.getElementById('popUp');
 
 popUp.addEventListener('click', function(){
     alert('In one second, you could see the cutest pets waiting for your love.');
+});
+
 });
